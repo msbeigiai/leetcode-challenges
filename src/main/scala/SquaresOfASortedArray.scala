c830c2f71f8f0374adc9f0581b5f65f11073d081
msbeigiai <msbeigi83@gmail.com>
2023-11-14 00:37:24 +0200

    Solution with built in sorted function

--- src/main/scala/SquaresOfASortedArray.scala
+++ src/main/scala/SquaresOfASortedArray.scala
@@ -7,9 +7,18 @@ object SquaresOfASortedArray extends App {
       .sorted
   }
 
-//   val sorted = sortedSquares(Array(-4, -1, 0, 3, 10))
-//   println(sorted)
+  class SortedSquare(val nums: Array[Int]) {
+    override def toString(): String = nums.toList.mkString("[", ", ", "]")
+  }
 
-  sortedSquares(Array(-4, -1, 0, 3, 10)).foreach(print)
+  object SortedSquare {
+    def apply(nums: Array[Int]) = new SortedSquare(nums.map(n => n * n).sorted)
+  }
+
+  val sorted = SortedSquare(Array(-7, -3, 2, 3, 11))
+  println(sorted)
+
+//   val sortedSquares = new SortedSquare(Array(-4, -1, 0, 3, 10)).sorteSquare()
+//   println(sortedSquares)
 
 }
